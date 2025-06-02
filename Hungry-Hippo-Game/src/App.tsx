73cc2578c4d3b960296beab6229bc09ddc4238c3
Omais <tun70323@temple.edu>
2025-06-02 13:24:24 -0400

    Added current fruit indicator

--- Hungry-Hippo-Game/src/App.tsx
+++ Hungry-Hippo-Game/src/App.tsx
@@ -44,13 +44,35 @@ function App()
        });
     };
 
+    // Get the current selected fruit (top of stack)
+    const currentFruit = fruitStack.length > 0 ? fruitStack[0] : null;
+
     return (
         <div id="app">
             <AacInterface onFruitSelected={handleSelectedFruit}/>
-            <PhaserGame ref={phaserRef} />
-            <div>
+            <div className="game-container">
+                <PhaserGame ref={phaserRef} />
+                
+                <div className="current-fruit-indicator">
+                    <h3>Current Fruit to Eat:</h3>
+                    {currentFruit ? (
+                        <>
+                            <img
+                                src={currentFruit.imagePath}
+                                alt={currentFruit.name}
+                                className="current-fruit-image"
+                            />
+                            <p className="current-fruit-name">{currentFruit.name}</p>
+                        </>
+                    ) : (
+                        <p className="current-fruit-placeholder">No Fruit Selected</p>
+                    )}
+                </div>
+                
                 <div>
-                    <button className="button" onClick={addSprite}>Add New Sprite</button>
+                    <div>
+                        <button className="button" onClick={addSprite}>Add New Sprite</button>
+                    </div>
                 </div>
             </div>
         </div>
