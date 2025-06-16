114bfbafd506eb623d7e0b7e7ec50026ab849823
Mohammed Karim <makarim913@temple.edu>
2025-06-15 23:31:23 -0400

    added router file and modified routes in apptsx

--- Hungry-Hippo-Game/src/App.tsx
+++ Hungry-Hippo-Game/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import GamePage from './pages/GamePage/GamePage';
+import AppRoutes from './router';
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/gamepage" element={<GamePage />} />
-      </Routes>
+      
+        <AppRoutes/>
+     
     </BrowserRouter>
   );
 }
