d82d02f7374fef4c6684071bcccaa9e433db558c
liu-jasmine <71993034+liu-jasmine@users.noreply.github.com>
2025-06-24 10:09:00 -0400

    Updated App Routes

--- Hungry-Hippo-Game/src/App.tsx
+++ Hungry-Hippo-Game/src/App.tsx
@@ -3,6 +3,9 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import GamePage from './pages/GamePage/GamePage';
 import Presenter from './pages/PresenterPage/Presenter';
 import RoleSelect from './pages/RoleSelection/RoleSelect';
+import PhaserPage from './pages/PhaserPage/PhaserPage';
+import AacPage from './pages/AacPage/AacPage';
+
 import { Navigate } from 'react-router-dom';
 
 function App() {
@@ -10,11 +13,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/gamepage/:sessionId/:userId" element={<GamePage />} />
-        {/* delete /gamepage later, for testing */}
-        <Route path="/gamepage" element={<GamePage />} /> 
         <Route path="/presenter/:sessionId" element={<Presenter />} />
         <Route path="/roleselect/:sessionId" element={<RoleSelect />} />
+        <Route path="/hippo/:sessionId/:userId" element={<PhaserPage />} />
+        <Route path="/aac/:sessionId/:userId" element={<AacPage />} />
 
         {/* Redirect to landing page */}
         <Route path="*" element={<Navigate to="/" replace />} />
