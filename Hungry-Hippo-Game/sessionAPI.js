5e6924d449c72c3c8f5f8ec59a63e5f1e0690a54
Kostandin Jorgji <tug74295@temple.edu>
2025-06-30 09:35:36 -0400

    Update

--- Hungry-Hippo-Game/sessionAPI.js
+++ Hungry-Hippo-Game/sessionAPI.js
@@ -11,7 +11,8 @@ const wss = new WebSocket.Server({ noServer: true });
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://project-acc-hungry-hippos.vercel.app'
+  'https://www.draexico.com',
+  'https://project-acc-hungry-hippos-9wsu.vercel.app'
 ];
 
 server.on('upgrade', (request, socket, head) => {
