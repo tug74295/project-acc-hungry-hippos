201554b4e84b576ee435f99e018e93a6de560a15
Kostandin Jorgji <tug74295@temple.edu>
2025-07-02 11:47:04 -0400

    Update README how to run

--- Hungry-Hippo-Game/sessionAPI.js
+++ Hungry-Hippo-Game/sessionAPI.js
@@ -11,6 +11,10 @@ const wss = new WebSocket.Server({ noServer: true });
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:3003',
+  'http://localhost:3004',
+  'http://localhost:3005',
   'https://project-acc-hungry-hippos.vercel.app'
 ];
 
