011c23a1ccb599e43bcf43deeca47a27ba459375
Kostandin Jorgji <tug74295@temple.edu>
2025-07-27 16:48:55 -0400

    url update

--- Hungry-Hippo-Game/sessionAPI.js
+++ Hungry-Hippo-Game/sessionAPI.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   'http://localhost:3003',
   'http://localhost:3004',
   'http://localhost:3005',
-  'https://project-acc-hungry-hippos.vercel.app'
+  'https://www.draexico.com'
 ];
 
 server.on('upgrade', (request, socket, head) => {
